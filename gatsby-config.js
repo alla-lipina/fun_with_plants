417f04242b785bde7985081f333403b21198f732
Alla Lipina <alla.lipina@xing.com>
2020-04-21 14:12:20 +0200

    use blog theme from gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,25 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
+  pathPrefix: "/fun_with_plants",
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+      resolve: `gatsby-theme-blog`,
+      options: {},
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Fun with Plants`,
+    author: `Alla`,
+    description: `Sharing my experience of growing, caring and sometimes unfortunately killing plants at home.`,
+    social: [
+      {
+        name: `Instagram`,
+        url: `https://www.instagram.com/fun_with_plants/`,
+      },
+      {
+        name: `Twitter`,
+        url: `https://www.instagram.com/fun_with_plants/`,
+      },
+    ],
+  },
 }
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    Moin and welcome to my blog. I am <Styled.a href="http://example.com/">Alla</Styled.a> 👩‍🌾.
+    <br />
+    Here I share my experience of growing plants at home.
+  </Fragment>
+)
